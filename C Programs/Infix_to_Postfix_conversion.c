f046c8d096f2cd55d25944dd5c125a8908d6a3d6
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-10-09 11:14:22 +0530

    Add files via upload

---
+++ C Programs/Infix_to_Postfix_conversion.c
@@ -0,0 +1,64 @@
+/*ABHISHEK JOSHI        20011988        SEC:B */
+#include<stdio.h>
+char stack[100];
+int top=-1;
+int preference (int c)
+{
+    if(c == '(')
+        return 1;
+    if(c=='+' || c=='-')
+        return 2;
+    if(c=='*' || c=='/')
+        return 3;
+}
+int main()
+{
+    char s[100];
+    int i=0;
+    printf("Enter INFIX expression: ");
+    scanf("%s",s);
+    
+    printf("Showing its POSTFIX expression: ");
+    while(s[i]!='\0')
+    {
+        if( (s[i]>='a'&&s[i]<='z') || (s[i]>='A'&&s[i]<='Z') || (s[i]>='1'&&s[i]<='9') )
+            printf("%c ",s[i]);
+            
+        else if(s[i]=='(')
+        {
+            ++top;
+            stack[top]=s[i];
+        }
+        
+        else if(s[i]==')')
+        {
+            while(stack[top]!='(')
+            {
+                printf("%c ",stack[top]);
+                --top;
+            }
+            --top;
+        }
+        
+        else
+        {
+            while(preference(stack[top])>preference(s[i]))
+            {
+                printf("%c ",stack[top]);
+                --top;
+            }
+            top++;
+            stack[top]=s[i];
+        }
+        
+        i++;
+    }
+    
+    while(top != -1)
+    {
+        printf("%c ",stack[top]);
+        --top;
+    }
+    return 0;
+}
+
