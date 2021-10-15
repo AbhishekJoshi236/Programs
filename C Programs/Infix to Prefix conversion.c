030e6f2e9a75cb2a3ede7062ec2d75b90942b152
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-10-15 06:44:20 +0530

    Update Infix to Prefix conversion.c

--- C Programs/Infix to Prefix conversion.c
+++ C Programs/Infix to Prefix conversion.c
@@ -1,13 +1,14 @@
-/*  ABHISHEK JOSHI  */
+/* ABHISHEK JOSHI  */
+
 #include<stdio.h>
 #include<string.h>
 
-char stack[100];
+char a[100];
 int top=-1;
 
 int preference (int c)
 {
-    if(c == ')')
+    if(c == '(')
         return 1;
     if(c=='+' || c=='-')
         return 2;
@@ -15,42 +16,55 @@ int preference (int c)
         return 3;
 }
 
-int main()
-{
-    char s[100],c[100];
-    int i=0,t=0;
-    
-    printf("Enter INFIX expression: ");
-    scanf("%s",s);
-        
-    int d=strlen(s)-1;
-    for(int j=0;j!=d;d--,j++)
+void reverse(char s[],int d)
+{ 
+    for(int j=0;j!=d;j++,d--)
     {
         char b=s[d];
         s[d]=s[j];
         s[j]=b;
     }
+}
+
+int main()
+{
+    char s[100],c[100];
+    int e,t=0;
     
-    printf("Showing its PREFIX expression:  ");
-    while(s[i]!='\0')
-    { 
-        if( (s[i]>='a' && s[i]<='z') ||(s[i]>='A' && s[i]<='Z') ||(s[i]>='0' && s[i]<='9') )
-        {  
+    printf("Enter INFIX expression: ");
+    scanf("%s",s);
+    e=strlen(s);
+    
+    for(int j=0;j<e;j++)
+    {
+        if(s[j]==')')
+            s[j]='(';
+        else if(s[j]=='(')
+            s[j]=')';
+    }
+    
+    reverse(s,e-1);
+    printf("Showig its PREFIX expression:  ");
+    
+    for(int i=0;s[i]!='\0';i++)
+    {
+        if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9') )
+        {
             c[t]=s[i];
-            t++;   
-        }
-        
-        else if(s[i]==')')
-        {  
-            ++top;
-            stack[top]=s[i];
+            t++;  
         }
         
         else if(s[i]=='(')
         {
-            while(stack[top]!=')')
+            ++top;
+            a[top]=s[i];
+        }
+        
+        else if(s[i]==')')
+        {
+            while(a[top]!='(')
             {
-                c[t]=stack[top];
+                c[t]=a[top];
                 t++;
                 --top;
             }
@@ -59,33 +73,29 @@ int main()
         
         else
         {
-            while( preference(s[i]) <= preference(stack[top]) )
+            while(preference(s[i]) <= preference(a[top]) )
             {
-                c[t]=stack[top];
+                c[t]=a[top];
                 --top;
                 t++;
             }
+            
             top++;
-            stack[top]=s[i];
+            a[top]=s[i];
         }
-    i++;
-  }
-    while(top != -1)
-    { 
-        c[t]=stack[top];
-        --top;
-        t++;
     }
-    
-    for(int j=0,d=t-1;j!=d;d--,j++)
-    { 
-        char b=c[d];
-        c[d]=c[j];
-        c[j]=b;
-    }
-    
-    for(int j=0;j<t;j++)
-      printf("%c",c[j]);
-      
+ 
+while(top != -1)
+{
+    c[t]=a[top];
+    --top;
+    t++;
+}
+
+reverse(c,t-1);
+
+for(int j=0;j<t;j++)
+     printf("%c",c[j]);
+     
   return 0;
 }
